bd98cf6f31c585a975702e506e24942c9728b288
josephlanders <40991648+josephlanders@users.noreply.github.com>
2018-08-19 13:02:15 +1000

    Create webcache.c

---
+++ Algorithms and analysis Assignment 2/webcache.c
@@ -0,0 +1,341 @@
+/****************************************************************************
+ * COSC1285 Algorithms and Analysis Assignment #2
+ * Author:            Joseph Peter Landers
+ * Student Number:    s3163776@student.rmit.edu.au
+ * Alternate contact: josephlanders@gmail.com
+ * Yallara Username:  jlanders / s3163776
+ * 
+ * This file is the back-end code that sits between the driver and the ADT
+ * specific code.
+ ****************************************************************************/
+
+#include "assign.h"
+#include "utility.h"
+#include "webcache.h"
+#include "resourcenode-main.h"
+#include "resourcenode-hash.h"
+#include "resourcenode-queue.h"
+
+/*
+ * Initialise the webcache struct 
+ * 
+ * wc is a pointer to the webcache struct.
+ * cachesize is the size of the cache.
+ * 
+ * returns TRUE or FALSE to indicate success or failure of the operation
+ */
+int WebCacheInit(WebCache *wc, int cachesize)
+{
+    int i = 0;
+
+    /*
+     * Set the pointers to the first & last resource node 
+     * in the queue to NULL 
+     */
+    wc -> head = NULL;
+    wc -> tail = NULL;
+    wc -> bytesused = 0;
+    wc -> cachesize = cachesize;
+    wc -> pagehitcount = 0;
+    wc -> pagerequestcount = 0;
+    wc -> bytehitcount = 0;
+    wc -> byterequestcount = 0;
+    wc -> itemcount = 0;
+    wc -> evictedcount = 0;
+    wc -> totalaccesstime = 0;
+    wc -> totalinsertiontime = 0;
+
+    /*
+     * Allocate memory for an array of pointers to resources 
+     * This means allocate the hash table.
+     */
+    if ((wc -> hashtable = (resourcenode **)
+                           malloc (HASHTABLE_SIZE * sizeof(resourcenode *)))
+            == NULL)
+    {
+        return FALSE;
+    }
+
+    /*
+     * Not sure if hashtable pointers are set to NULL on malloc
+     * so NULL to be safe 
+     */
+    for (i = 0; i < HASHTABLE_SIZE; i++)
+    {
+        wc -> hashtable[i] = NULL;
+    }
+
+    return TRUE;
+}
+
+/*
+ * Get a resource from the webcache 
+ * 
+ * wc is a pointer to the webcache struct.
+ * resource is a pointer to the name of the resource.
+ * bytes is the size of the resource.
+ * 
+ * returns TRUE or FALSE to indicate cache HIT or MISS.
+ */
+int WebCacheRetrieve(WebCache *wc,
+                     char *resource,
+                     int bytes)
+{
+    /* Stores status of retrieve function call */
+    int response = FALSE;
+
+    /*
+     * The following code is system specific
+     * 
+     * The code retrieves from the webcache and
+     * works out how long the operation took, 
+     * adding the time to a cumulative count 
+     */
+
+#if defined(linux) || defined (__linux)
+
+    clock_t ticks1 = { 0 };
+    clock_t ticks2 = { 0 };
+
+    ticks1 = clock();
+    ticks2 = ticks1;
+
+    response = retrieveResourceNode(wc, resource, bytes);
+    ticks2 = clock();
+
+    wc -> totalaccesstime += (ticks2 - ticks1);
+
+#endif
+
+#if defined(sun) || defined(__sun)
+
+    hrtime_t ticks1 = { 0 };
+    hrtime_t ticks2 = { 0 };
+
+    ticks1 = gethrtime();
+    ticks2 = ticks1;
+
+    response = retrieveResourceNode(wc, resource, bytes);
+    ticks2 = gethrtime();
+
+    wc -> totalaccesstime += (ticks2 - ticks1);
+
+#endif
+
+    return response;
+
+}
+
+/*
+ * Insert a resource into the webcache 
+ * 
+ * wc is a pointer to the webcache struct.
+ * resource is a pointer to the name of the resource.
+ * bytes is the size of the resource.
+ * 
+ * returns TRUE or FALSE to indicate success or failure.
+ */
+int WebCacheInsert(WebCache *wc,
+                   char *resource,
+                   int bytes)
+{
+    /* Stores status of retrieve function call */
+    int response = 0;
+
+    /*
+     * The following code is system specific
+     * 
+     * The code inserts into the webcache and
+     * works out how long the operation took, 
+     * adding the time to a cumulative count 
+     */
+#if defined(linux) || defined (__linux)
+
+    clock_t ticks1 = { 0 };
+    clock_t ticks2 = { 0 };
+
+    ticks1 = clock();
+    ticks2 = ticks1;
+
+    response = insertResourceNode(wc, resource, bytes);
+
+    ticks2 = clock();
+
+    /* / (CLOCKS_PER_SEC / 1000) needs to be done to convert to millseconds */
+    wc -> totalinsertiontime += (ticks2 - ticks1);
+
+#endif
+
+#if defined(sun) || defined(__sun)
+
+    hrtime_t ticks1 = { 0 };
+    hrtime_t ticks2 = { 0 };
+
+    ticks1 = gethrtime();
+    ticks2 = ticks1;
+
+    response = insertResourceNode(wc, resource, bytes);
+
+    ticks2 = gethrtime();
+
+    wc -> totalinsertiontime += (ticks2 - ticks1);
+
+#endif
+
+    return response;
+}
+
+/*
+ * Print the contents of the webcache 
+ * 
+ * wc is a pointer to the webcache struct
+ */
+void WebCachePrint(WebCache *wc)
+{
+    /* Iterate through queue printing */
+    printNodes(wc -> head);
+    return;
+}
+
+/* 
+ * Gets the total disk cost for inserting 
+ * into the webcache on cache misses
+ * 
+ * wc is the webcache struct
+ * 
+ * value returned is in ms
+ */
+double getInsertionTime(WebCache * wc)
+{
+	double cost = 0;
+	unsigned long long int misscount = wc -> pagerequestcount 
+	- wc -> pagehitcount;
+	
+    /* 
+     * Calculate seek time in ms
+     * 
+     * 11ms is the seek time for a single request
+     */ 
+    cost = (double) 11 * misscount;
+    
+   
+    /* 
+     * Calculate Transfer time in ms
+     * 
+     * 67.5 MB/s (megabytes per second) is the transfer speed 
+     */ 
+    cost += (wc -> byterequestcount * 1000) 
+          / (double) (67.5 * 1024 * 1024);
+    
+    /* 
+     * Calculate rotational delay in ms
+     * 
+     * 5400 rpm is the disk rotation speed 
+     */
+    cost += (1 * 60 * 1000 * misscount) / (double) (2 * 5400);
+        
+    /* 
+     * Calculate latency in ms
+     * 
+     * 5.5 ms is the latency for one file read 
+     */ 
+    cost += (double) 5.5 * (wc -> pagerequestcount - wc -> pagehitcount);
+    
+    return cost;
+}
+
+/*
+ * Print the statistics of the webcache 
+ *
+ * wc is a pointer to the webcache struct 
+ */
+void WebCacheDisplayStats(WebCache *wc)
+{
+    unsigned long long int pagehitrate = 0;
+    unsigned long long int bytehitrate = 0;
+
+    double accesstime = wc -> totalaccesstime;
+    double insertiontime = wc -> totalinsertiontime;
+    
+    double diskcost = 0;
+    
+    diskcost = getInsertionTime(wc);
+
+    /* System specific code to calculate correct access and insertion times */
+#if defined (__linux) || defined (linux)
+
+    accesstime /= (CLOCKS_PER_SEC / 1000);
+    insertiontime /= (CLOCKS_PER_SEC / 1000);
+#endif
+
+#if defined (__sun) || defined (sun)
+
+    accesstime /= 1000000;
+    insertiontime /= 1000000;
+#endif
+
+    /* Calculate page hit rate */
+    if (wc -> pagerequestcount != 0)
+    {
+        pagehitrate = (wc -> pagehitcount * 100) / wc -> pagerequestcount;
+    }
+
+    /* Calculate byte hit rate */
+    if (wc -> byterequestcount != 0)
+    {
+        bytehitrate = (unsigned long long int) (wc -> bytehitcount * 100)
+                      / wc -> byterequestcount;
+    }
+
+    /* Print out relevant statistics */
+
+    printf("\n Printing Statistics");
+    printf("\n -------------------");
+    printf(STR_STATS_PAGE_HIT_COUNT, wc -> pagehitcount);
+    printf(STR_STATS_PAGE_REQUEST_COUNT, wc -> pagerequestcount);
+    printf(STR_STATS_PAGE_HIT_PERCENT, pagehitrate);
+    printf(STR_STATS_BYTE_HIT_COUNT, wc -> bytehitcount);
+    printf(STR_STATS_BYTE_REQUEST_COUNT, wc -> byterequestcount);
+    printf(STR_STATS_BYTE_HIT_PERCENT,  bytehitrate);
+    printf(STR_STATS_COUNT, wc -> itemcount);
+    printf(STR_STATS_EVICTION, wc -> evictedcount);
+    printf(STR_STATS_ACCESS_TOTAL,  accesstime);
+    printf(STR_STATS_ACCESS_AVERAGE, accesstime
+           / wc -> pagerequestcount);
+    printf(STR_STATS_INSERTION_TOTAL, insertiontime);
+    printf(STR_STATS_INSERTION_AVERAGE, insertiontime
+           / (wc -> pagerequestcount - wc -> pagehitcount));
+    /* Could also use wc - itemcount + wc - evicted count as divisor */
+    
+    printf(STR_STATS_DISK_INSERTION_COST, diskcost);
+    printf(STR_STATS_DISK_INSERTION_COST_AVG, diskcost 
+    /  (wc -> pagerequestcount - wc -> pagehitcount));
+
+    printf("\n\n");
+    fflush(stdout);
+}
+
+/*
+ * Free up the webcache struct memory 
+ * 
+ * wc is a pointer to the webcache struct 
+ * 
+ * returns TRUE or FALSE to indicate success or failure
+ */
+int WebCacheDestroy(WebCache *wc)
+{
+    /*
+     * If the queue head exists,
+     * this means there are items in the cache,
+     * so free them 
+     */
+    if (wc -> head != NULL)
+    {
+        freeResources(wc -> head);
+    }
+
+    /* Now we can free the hashtable safely */
+    free(wc -> hashtable);
+
+    return TRUE;
+}
